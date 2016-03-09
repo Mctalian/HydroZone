fc9c631655e2af745cfc98574b56ffdc6e770080
Robert <r-anderson.9@onu.edu>
2016-03-08 19:35:04 -0500

    Adds Grunt bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,21 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: ['pkg'],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+            }
+        },
+
         jshint: {
             options: {
                 reporter: require('jshint-stylish')
@@ -12,5 +27,6 @@ module.exports = function(grunt) {
     });
 
     // LOAD GRUNT PACKAGES
+    grunt.loadNpmTasks('grunt-bump');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 };
