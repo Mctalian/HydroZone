716fbbcb0ef4792724bd42acec44abf11343b46e
Robert <r-anderson.9@onu.edu>
2016-03-08 22:47:21 -0500

    Remove ability to pass port as arg
    Cucumber adds arguments to server start command which caused the port to
    be assigned to NaN and it would break the server.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || process.argv[2] || 8080;
+var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
 app.listen(port);
 console.log(`I can hear you on port ${port}!`);
+
+module.exports.port = port;
