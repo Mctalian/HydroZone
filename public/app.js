573a33f1503ec787efad5ab14b2d6d9706acbeb0
Robert <r-anderson.9@onu.edu>
2016-03-08 21:46:15 -0500

    Add Material Theming
    Now to start making it look pretty

--- public/app.js
+++ public/app.js
@@ -1,3 +1,13 @@
 angular.module("HydroZone", [
     'ngMaterial'
-]);
+])
+.config(
+    [
+        '$mdThemingProvider',
+        function($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('indigo')
+                .accentPalette('cyan');
+        }
+    ]
+);
