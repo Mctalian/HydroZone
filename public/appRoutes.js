4e6109ef4bc46e13069165965bc8bc8b3b0ba08e
Robert Anderson <r-anderson.9@onu.edu>
2016-03-09 18:25:14 -0500

    Move Initial Homepage text to template

---
+++ public/appRoutes.js
@@ -0,0 +1,21 @@
+angular.module('HydroZone')
+.config(
+    [
+        '$stateProvider', '$urlRouterProvider', '$locationProvider',
+        function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+            // Unknown URLS redirect to home URL
+            $urlRouterProvider.otherwise('/');
+
+            $stateProvider
+                // Home page
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'home/home.tpl.html'
+                });
+
+            // Remove '#' from URL
+            $locationProvider.html5Mode(true);
+        }
+    ]
+)
