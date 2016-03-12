ef371d2f094340ca2641d87fe4f19c8e35b37d79
Robert <r-anderson.9@onu.edu>
2016-03-12 13:40:24 -0500

    Center Home Page and remove buttons
    It automatically checks validity in stages. First it checks the format,
    then it sends it to the server. 500ms after the user stops typing or
    pasting, it will run these checks. This is the same as  it used to be,
    but takes out the need to press the buttons.
    
    Also adds a loading spinner so the user knows things are happening.

--- public/home/homeCtrl.js
+++ public/home/homeCtrl.js
@@ -6,11 +6,14 @@ angular.module('HydroZone')
             var ctrl = this;
 
             ctrl.regex = rachioSvc.tokenRegExp;
+            ctrl.disableAll = false;
 
             ctrl.submit = function() {
+                ctrl.disableAll = true;
                 rachioSvc.validateToken(ctrl.apiToken).then(function() {
                     $state.go('dash');
                 }).catch(function() {
+                    ctrl.disableAll = false;
                     $mdToast.showSimple('The API Token you supplied is not authorized');
                 });
             };
