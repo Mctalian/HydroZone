59f236931f620d96a526d8f395f2d2359a0bcd7b
Robert Anderson <r-anderson.9@onu.edu>
2016-03-13 20:59:33 -0400

    Change up the colors
    I found a palette, might as well try to use it.

--- public/app.js
+++ public/app.js
@@ -6,9 +6,26 @@ angular.module('HydroZone', [
     [
         '$mdThemingProvider',
         function($mdThemingProvider) {
+            var rachioBlue = $mdThemingProvider.extendPalette('light-blue', {
+                '500': '00A7E1',
+                '800': '01579B',
+                '900': '0E4475',
+                'contrastDefaultColor': 'light',
+                'contrastDarkColors': [
+                    '50',
+                    '100',
+                    '200',
+                    '300',
+                    '400',
+                    'A100',
+                    'A200',
+                    'A400'
+                ]
+            });
+            $mdThemingProvider.definePalette('rachio-blue', rachioBlue);
             $mdThemingProvider.theme('default')
-                .primaryPalette('indigo')
-                .accentPalette('cyan');
+                .primaryPalette('rachio-blue')
+                .accentPalette('blue-grey');
         }
     ]
 );
